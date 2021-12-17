2d067de649964dbddeb1ffd71cf90f5df777966e
jon-cbar <jon-cbar@outlook.com>
2021-12-17 08:41:04 -0300

    env port on python

--- backend/python/api.py
+++ backend/python/api.py
@@ -5,7 +5,7 @@ from json import dumps
 from os import environ
 
 host_name = "0.0.0.0"
-host_port = environ.get('PORT')
+host_port = int(environ.get('PORT'))
 data_path = 'data.txt'
 
 def get_data():
